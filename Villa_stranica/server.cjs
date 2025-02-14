b88bfc3294118a61f684bef15c85f9f3f6acb15e
manje22 <uvodicmariela@gmail.com>
2025-02-14 18:22:01 +0100

    dodala konekciju na bazu

--- Villa_stranica/server.cjs
+++ Villa_stranica/server.cjs
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+
 
 const app = express();
 
@@ -11,4 +13,19 @@ app.use(express.json());
 const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server sluša zahtjeve na portu ${PORT} (๑˃ᴗ˂)ﻭ`);
+});
+
+//spajam se na bazu
+mongoose.connect('mongodb://127.0.0.1:27017/VillaDaniela',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', (error) =>{
+    console.error('Greska pri spajanju:', error);
+});
+db.once('open', function(){
+    console.log('Spojeni smoo na MongoDB bazu');
 });
