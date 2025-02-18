8012924618e2e08c96542c8d8b4239f76338b975
manje22 <uvodicmariela@gmail.com>
2025-02-18 10:14:03 +0100

    dodala schemu za korisnika i dodala rute za prijavu i registraciju

--- Villa_stranica/server.cjs
+++ Villa_stranica/server.cjs
@@ -2,9 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 
 const app = express();
+
+const saltRunde = 10;
 
 //MIDDLEWARE
 app.use(cors());
@@ -16,10 +19,8 @@ app.listen(PORT, () => {
 });
 
 //spajam se na bazu
-mongoose.connect('mongodb://127.0.0.1:27017/VillaDaniela',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+
+mongoose.connect('mongodb://127.0.0.1:27017/VillaDaniela');
 
 const db = mongoose.connection;
 
@@ -29,3 +30,47 @@ db.on('error', (error) =>{
 db.once('open', function(){
     console.log('Spojeni smoo na MongoDB bazu');
 });
+
+
+//Sve scheme
+const { Schema } = mongoose;
+
+const korisnikShema = new Schema({
+    name: String,
+    surname: String,
+    email: { type: String, unique: true },
+    password: String,
+});
+  
+
+const Korisnik = mongoose.model("Korisnik", korisnikShema);
+
+
+
+
+//rute
+app.post('/registracija', async (req, res)=>{
+    try{
+        const hashPassword = await bcrypt.hash(req.body.password, saltRunde);
+        const noviKorisnik = new Korisnik({...req.body, password: hashPassword});
+        await noviKorisnik.save();
+        res.status(201).send('Korinsik uspjesno registriran');
+    } catch(error){
+        res.status(500).send(error.message);
+    }
+});
+
+app.post('/prijava', async (req, res) => {
+    try {
+      const korisnikBaza = await Korisnik.findOne({ email: req.body.email });
+      if (korisnikBaza && await bcrypt.compare(req.body.password, korisnikBaza.password)) {
+        res.send('Prijava uspješna');
+        // Stvaranje login sesije ili tokena
+      } else {
+        res.status(401).send('Neispravni podaci za prijavu');
+      }
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+});
+  
